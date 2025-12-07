73fa490b6e6c4e604468de4348d35e419c822744
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-12-07 21:52:49 +0530

    clerk with convex

---
+++ my-app/app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+
+function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      {children}
+    </div>
+  )
+}
+
+export default layout
---
+++ my-app/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+
+import { SignUp } from '@clerk/nextjs';
+
+function page() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+          <SignUp />
+    </div>
+  )
+}
+
+export default page
