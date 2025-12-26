134709d357fc4602ea0ef19fe4f0b25e685ef798
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-12-27 00:00:03 +0530

    fix

--- my-app/convex/feed.ts
+++ my-app/convex/feed.ts
@@ -5,6 +5,11 @@ export const getFeed=query({
         limit:v.optional(v.number())
     },
     handler:async(ctx , args)=>{
+        const limit=args.limit || 10;
+
+        const allPosts=await ctx.db.query("posts").filter((q)=>q.eq(q.field("status"),"published")).order("desc").take(limit+1);
+
+        const  hasmore=false;
 
     }
 })
