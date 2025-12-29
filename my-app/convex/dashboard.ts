9dde7c42d2c6eda6aa020a28ac118f4aa9472f05
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-12-30 01:28:53 +0530

    dashboard

--- my-app/convex/dashboard.ts
+++ my-app/convex/dashboard.ts
@@ -1,1 +1,19 @@
 // // Get dashboard analytics for the authenticated user
+
+import { query } from "./_generated/server";
+
+export  const getAnalytics=query({
+    handler:async (ctx)=>{
+
+        const identity=await ctx.auth.getUserIdentity();
+        
+        const user=await ctx.db.query("users").filter(q=>q.eq(q.field("tokenIdentifier"),identity?.tokenIdentifier)).unique();
+
+        if(!user){
+            return null;
+        }
+
+        
+
+    }
+})
