85a0d979878048d1847161db18139081abef7b2b
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-12-31 22:58:35 +0530

    fix

--- my-app/components/header.tsx
+++ my-app/components/header.tsx
@@ -26,7 +26,7 @@ if (path.split("/").length >= 2 && path !== "/") {
 }
   return (
     <header className ="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 w-full max-w-3xl px-4">
-        <div className ="backdrop-blur-md bg-white/10 border border-white/20 rounded-full px-4 sm:px-6 md:px-8  py-3 flex items-center justify-between gap-2">
+        <div className ="backdrop-blur-md bg-white/10 border border-white/20 rounded-full px-4 sm:px-6 md:px-9  py-4 flex items-center justify-between gap-2">
         <Link href={isAuthenticated ? "/feed" : "/"} className ="flex shrink-0">
             <Image src="/logo.png" alt="Creatr  Logo" width={96} height={32} className  ="h-8 sm:h-10 w-auto object-contain" />
         </Link>
@@ -45,7 +45,10 @@ if (path.split("/").length >= 2 && path !== "/") {
                     className="text-white font-medium transition-all duration-300 hover:text-purple-300 cursor-pointer"
                     >
                     Testimonials
-                    </Link>
+                  </Link>
+                  <Link  href="/dashboard" className="text-white font-medium transition-all duration-300 hover:text-purple-300 cursor-pointer border border-white/30 rounded-full px-4 py-1">
+                     Dashboard
+                  </Link>
 
                 </div>
 
